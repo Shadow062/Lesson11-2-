37cd665093784c024bab87a7e1a338e63897d68d
Ali <a.dadawov@outlook.com>
2020-04-28 02:20:11 +0300

    Promise11

--- js/script.js
+++ js/script.js
@@ -2,23 +2,25 @@ let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
 inputRub.addEventListener('input', () => {
-    return new Promise(function (resolve, reject) {
-        let request = new XMLHttpRequest();
+    function getMoney() {
+        return new Promise((resolve, reject) => {
+            let request = new XMLHttpRequest();
 
-        request.open('GET', 'js/current.json');
-        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-        request.send();
+            request.open('GET', 'js/current.json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
 
-        request.addEventListener('readystatechange', function () {
-            if (request.readyState === 4 && request.status == 200) {
-                let data = JSON.parse(request.response);
-
-                inputUsd.value = inputRub.value / data.usd;
-            } else {
-                reject("Что-то пошло не так!");
-            }
+            request.addEventListener('readystatechange', function () {
+                if (request.readyState === 4 && request.status == 200) {
+                    let data = JSON.parse(request.response);
+                    resolve(data);
+                } else if (request.status !== 200) {
+                    reject();
+                }
+            });
         });
-    });
-
-
+    }
+    getMoney()
+        .then((data) => inputUsd.value = inputRub.value / data.usd.toFixed())
+        .catch(() => inputUsd.value = "Что-то пошло не так!");
 });
